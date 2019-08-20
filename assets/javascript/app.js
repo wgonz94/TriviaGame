519bafbf5220ea5a77e5a463a235b959a470efcb
William Gonzales <wgonz94@yahoo.com>
2019-08-20 11:41:33 -0700

    added functions to js to sort out for pseudo-coding. small changes to index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,3 +27,32 @@ function decrement() {
 
   }
 }
+// EXTRA CODE TO LOOK AT
+
+// setTimeout(fiveSeconds, 1000 * 5);
+// setTimeout(tenSeconds, 1000 * 10);
+// setTimeout(timeUp, 1000 * 15);
+
+// //  Step 3:
+// //  Fill in the blanks to these functions.
+// function fiveSeconds() {
+//   // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+//   // console log 10 seconds left
+//   $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+//   console.log("10 seconds left");
+// }
+
+// function tenSeconds() {
+//   // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
+//   // console log 5 seconds left
+//   $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
+//   console.log("5 seconds left");
+// }
+
+// function timeUp() {
+//   // in the element with an id of time-left add an h2 saying Time's Up!
+//   // console log done
+//   console.log("done");
+//   $("#time-left").append("<h2>Time's Up!</h2>");
+//   console.log("time is up");
+// }
